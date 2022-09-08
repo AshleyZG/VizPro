bab36370546bedee51b3012b266e1ec34721abfb
AshleyZG <gsarazh@gmail.com>
2022-09-07 21:53:51 -0400

    remove plugin

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import pluginVizPro from './VizPro';
+// import pluginVizPro from './VizPro';
 
 /**
  * Initialization data for the VizPro extension.
@@ -16,4 +16,5 @@ const plugin: JupyterFrontEndPlugin<void> = {
   }
 };
 
-export default [plugin, pluginVizPro];
+export default plugin;
+// export default [plugin, pluginVizPro];
